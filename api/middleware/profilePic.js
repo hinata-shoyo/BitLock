99e0a070e615e59864077a4f4cefedfec74f02a4
abhishek <avsharma4500@gmail.com>
2024-03-26 12:28:18 +0530

    path added

--- api/middleware/profilePic.js
+++ api/middleware/profilePic.js
@@ -1,4 +1,5 @@
 const multer = require("multer");
+const path = require("path")
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -6,9 +7,21 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, `${file.fieldname}_${uniqueSuffix}${path.extname(file.originalname)}`);
   },
 });
-const upload = multer({ storage });
+
+const fileFilter = (req, file, cb) => {
+  const allowedFile = [ "image/jpeg", "image/png", "image/jpg"]
+  if(allowedFile.includes(file.mimetype)){
+    cb(null, true)
+  }else{
+    cb(null, false)
+  }
+}
+
+const upload = multer({ storage, fileFilter });
+
+
 
 module.exports = upload;
