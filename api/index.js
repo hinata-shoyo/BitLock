183d22c306f769f99969b52cdc135d9d3c4c13e0
abhishek <avsharma4500@gmail.com>
2024-03-29 14:54:24 +0530

    yuhgh

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const app = express();
 
 app.use(cors());
-app.use(express.static(__dirname+'uploads'))
+app.use(express.static(__dirname+'./uploads'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/user", userRouter);
